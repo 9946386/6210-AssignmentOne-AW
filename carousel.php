fb42c745cd1a5e6592a2b55dbb6c71f74b67caa8
Anya Webb <9946386@student.toiohomai.ac.nz>
2020-04-13 14:09:12 +1200

    Finished Code

---
+++ carousel.php
@@ -0,0 +1,41 @@
+<div class="container" style="width: 80% !important;">
+    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+        <img src="SCP002.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+            <h5>SCP Subject 002</h5>
+            <p>SCP-002 in its containment area</p>
+        </div>
+        </div>
+        <div class="carousel-item">
+        <img src="SCP004.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+            <h5>SCP Subject 004</h5>
+            <p>SCP-004-1</p>
+        </div>
+        </div>
+        <div class="carousel-item">
+        <img src="SCP005.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+            <h5>SCP Subject 005</h5>
+            <p>A close up of SCP-005</p>
+        </div>
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+    <br><br>
+    </div>
+</div>
--- header.php
+++ header.php
@@ -53,11 +53,11 @@
         </nav>
     </div>
 
-    <!-- Welcome blurb -->
+    <!-- Welcome blurb
     <div class="container">
         <p class='text-center'><br>Welcome Agent,
                                <br>Use the links above to view subject files or enter new subject data</p>
-    </div>
+    </div> -->
            
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
